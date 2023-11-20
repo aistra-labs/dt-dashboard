51f4100d53bdecbbd880c29cba292deb485e2ed4
Arif <arif.rasool@aistra.com>
2023-11-20 14:47:57 +0530

    initial project set-up

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+// in src/App.tsx
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
+import { dataProvider } from "./dataProvider";
+
+export const App = () => <Admin dataProvider={dataProvider}></Admin>;
